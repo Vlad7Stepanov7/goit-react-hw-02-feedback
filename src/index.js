9e4497ddb4a53e4c5b12f0255f0fa384a78c5761
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-11-29 17:18:23 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { theme } from 'utils/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme= {theme}>
+    <ThemeProvider theme={theme}>
       <App />
       </ThemeProvider>
   </React.StrictMode>
