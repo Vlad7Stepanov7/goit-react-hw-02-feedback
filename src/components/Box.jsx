aec98ed154438b8fd4741371641009666c610f27
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-11-30 09:11:54 +0200

    виджет отзывов

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -10,7 +10,7 @@ import {
     position
 } from 'styled-system'
 
-export const Box = styled('div')(
+const Box = styled('div')(
     color,
     space,
     layout,
@@ -20,3 +20,5 @@ export const Box = styled('div')(
     border,
     position
 );
+
+export default Box;
